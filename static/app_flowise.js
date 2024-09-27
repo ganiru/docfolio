6494294512fd6d7bbca8c081e0e87b143d6813aa
Obi Ezeilo <110565429+obvar@users.noreply.github.com>
2024-09-26 22:57:53 -0500

    Fix for filename issue

--- static/app_flowise.js
+++ static/app_flowise.js
@@ -31,7 +31,6 @@ document.addEventListener("DOMContentLoaded", function () {
         return response.json();
       })
       .then((data) => {
-        console.log({ data });
         documentList.innerHTML = "";
         console.log({ data });
         if (data.documents.length === 0) {
@@ -55,10 +54,8 @@ document.addEventListener("DOMContentLoaded", function () {
           data.documents.forEach((doc) => {
             const docElement = document.createElement("div");
             // save the filename in a variable and truncate it if it's more than 20 characters
-            const filename =
-              doc.filename.length > 33
-                ? doc.filename.substring(0, 33) + "..."
-                : doc.filename;
+            const fn = doc.file_name;
+            const filename = fn.length > 33 ? fn.substring(0, 33) + "..." : fn;
             deleteSVG =
               '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>';
             // Convert bytes to kilobytes or megabytes
@@ -78,18 +75,14 @@ document.addEventListener("DOMContentLoaded", function () {
             docElement.innerHTML = `
                <div class="flex align-center gap-4 bg-[#f8fafb] px-4 py-3">
                     <div class="flex flex-1 flex-col justify-center">
-                        <p class="text-[#0e141b] text-base font-medium leading-normal" title="${
-                          doc.filename
-                        }">${filename}</p>
+                        <p class="text-[#0e141b] text-base font-medium leading-normal" title="${fn}">${filename}</p>
                         <p class="text-[#4f7396] text-sm font-normal leading-normal">${
                           doc.created_date && file_size
                             ? doc.created_date + ", " + file_size
                             : ""
                         }${total_pages}</p>
                     </div>
-                    <svg class="delete-button" data-filename="${
-                      doc.filename
-                    }" style="width:20px;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <svg class="delete-button" data-filename="${fn}" style="width:20px;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                     </svg>
 
